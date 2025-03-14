84b4f0c7df29b77c2dca2c2b2a1ca0f6d673af9d
Sarah Emilie <151790608+Velmist@users.noreply.github.com>
2025-03-14 11:44:15 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
 
+@app.route('/')
+def home():
+    return "¡Hola, mundo!"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) #puerto para el host
+    app.run(host="0.0.0.0", port=port)
